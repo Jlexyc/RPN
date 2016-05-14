8d2b34b3e8fa5bd13636857bf7f461ba2c58ecd5
Oleksii Kozulin <senior.jlexyc@gmail.com>
2016-05-14 19:19:50 +0300

    Added CocoaPods

--- RPN/ViewController.swift
+++ RPN/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Alamofire
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
     @IBOutlet weak var firstNameTextField: UITextField?
@@ -31,6 +32,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
                 self.tableView?.reloadData()
         }
+        
+        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
